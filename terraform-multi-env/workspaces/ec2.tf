0d0e2ad8882a471b276a083b1d50eaa5c564443d
shahul <shahul.met@gmil.com>
2024-10-01 14:59:43 +0530

    terraform

--- terraform-multi-env/workspaces/ec2.tf
+++ terraform-multi-env/workspaces/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-  #count = terraform.workspace == "prod" ? 2 : 1
+  # count = terraform.workspace == "prod" ? 2 : 1
   ami = "ami-09c813fb71547fc4f"
   instance_type = lookup(var.instance_type, terraform.workspace)
   vpc_security_group_ids = ["sg-01fb6cf8516e8a66a"]
