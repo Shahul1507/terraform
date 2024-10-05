bcd2acd49c8a929f37f93f3c786a0c883faa7d59
shahul <shahul.met@gmil.com>
2024-10-05 09:25:26 +0530

    ec2-module

--- ec2-module-demo/ec2.tf
+++ ec2-module-demo/ec2.tf
@@ -1,8 +1,9 @@
 module "ec2" {
     source = "../terraform-aws-ec2"
+    # source = "git::https://github.com/Shahul1507/terraform/terraform-aws-ec2.git"
     # these are variables expected module, not arguments of resource definition
     # You can also create variables or else you can directly use values here
     ami_id = var.ami_something
-    instance_type = "t3.large"
+    instance_type = "t3.micro"
     security_group_ids = ["sg-01fb6cf8516e8a66a"]
 }
